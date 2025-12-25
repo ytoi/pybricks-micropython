95a05f775d454856da9c9e3fc5830ac24e67dea3
Makoto Shimojima <shimojima_makoto@nias.ac.jp>
2025-12-25 21:53:10 +0900

    Poll USB every 1 msec.

--- lib/pbio/drv/usb/usb_stm32_serial.c
+++ lib/pbio/drv/usb/usb_stm32_serial.c
@@ -283,7 +283,7 @@ PROCESS_THREAD(pbdrv_usb_serial_process, ev, data) {
     PROCESS_BEGIN();
 
     pbdrv_stm32_usb_serial_init();
-    etimer_set(&timer, 5);
+    etimer_set(&timer, 1);
 
     for (;;) {
         PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_POLL || (ev == PROCESS_EVENT_TIMER && etimer_expired(&timer)));
